9dd282e4f6b716ff0928dbb7924591490614d93b
sakama <satoshi777v@gmail.com>
2013-01-30 02:23:34 +0900

    コマンド体系に合わせてspec_helper.rb更新

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,6 @@
 $:.unshift File.expand_path('../../lib', __FILE__)
 require 'chef'
 require 'chef/knife/niftycloud_server_create'
-require 'chef/knife/niftycloud_instance_data'
 require 'chef/knife/niftycloud_server_delete'
 require 'chef/knife/niftycloud_server_list'
+require 'chef/knife/niftycloud_image_list'
